0f82ae6c9ad8711b446f6e8115eb3167a09ce8b4
MollyMoile <marcus.hoile@gmail.com>
2014-06-05 23:40:50 +1000

    display message, no code response, clear input each try

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,10 +3,10 @@ function buttonPress(obj){
     var origWidth = obj.style.width;
     var origMarginLeft = obj.style.left;
     var origMarginTop = obj.style.marginTop;
-    obj.style.height = '161px';
-    obj.style.width = '161px';
-    obj.style.marginLeft = '7px';
-    obj.style.marginTop = '3px';
+    obj.style.height = '80px';
+    obj.style.width = '80px';
+    obj.style.marginLeft = '4px';
+    obj.style.marginTop = '2px';
     setTimeout(function(){
         obj.style.height = origHeight;
         obj.style.width = origWidth;
@@ -19,20 +19,34 @@ function perfect() {
     $ken.addClass('perfect');
     setTimeout(function() { $ken.removeClass('perfect'); }, 600);
     $ken.css("background-position", "-140px -800px");
+    $result.css("display", "block");
+}
+
+function destroyCode() {
+    $code.val(""); 
+}
+
+function noCode() {
+    alert("no Code bro");
 }
 
 
 function codeCheck() {
     var random = Math.random();
-    if (random <= 0.25) {
+    if ($code.val() == "") {
+        noCode();
+    } else if (random <= 0.25) {
         $ken.addClass('punch');
         setTimeout(function() { $ken.removeClass('punch'); }, 150);
+        destroyCode();
     } else if ((random > 0.25) && (random <= 0.5) ) {
         $ken.addClass('kick');
         setTimeout(function() { $ken.removeClass('kick'); }, 500);
+        destroyCode();
     } else if ((random > 0.25) && (random <= 0.75)) {
         $ken.addClass('hadoken');
         setTimeout(function() { $ken.removeClass('hadoken'); }, 500);               
+        destroyCode();
     } else {
         var delay = 500;
         $ken.removeClass('stance');
