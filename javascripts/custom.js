4124b0ab593b5040bd8613e6143ca797854e2f49
MollyMoile <marcus.hoile@gmail.com>
2014-06-12 23:01:01 +1000

    jump and then dragon punch

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,27 +1,65 @@
-function buttonPress(obj){
-    var origHeight = obj.style.height;
-    var origWidth = obj.style.width;
-    var origMarginLeft = obj.style.left;
-    var origMarginTop = obj.style.marginTop;
-    obj.style.height = '80px';
-    obj.style.width = '80px';
-    obj.style.marginLeft = '4px';
-    obj.style.marginTop = '2px';
-    setTimeout(function(){
-        obj.style.height = origHeight;
-        obj.style.width = origWidth;
-        obj.style.marginLeft = origMarginLeft;
-        obj.style.marginTop = origMarginTop;
-    }, 200);
-    setTimeout(function() {
-        // resetPlasma();
-        resetTetsu();
-        // codeCheck();
-        // hadoken();
-        tetsu();
+function startGame(){
+    if ($ken.hasClass('kick') || $ken.hasClass('punch') || $ken.hasClass('hadoken')) {
+        reboot();
+        buttonPress($bigButton);
+    } else {
+        reboot();
+        buttonPress($bigButton);
+        setTimeout(function() {
+            codeCheck();     
+        }, 200);
+    }
+}
 
+
+function buttonPress(obj) {
+    var origHeight = obj.css('height');
+    var origWidth = obj.css('width');
+    var origMarginLeft = obj.css('left');
+    var origMarginTop = obj.css('marginTop');
+    obj.css('height','80px');
+    obj.css('width', '80px');
+    obj.css('left', '4px');
+    obj.css('marginTop', '2px');
+    setTimeout(function(){
+        obj.css('height', origHeight);
+        obj.css('width', origWidth);
+        obj.css('left', origMarginLeft);
+        obj.css('marginTop', origMarginTop);
     }, 200);
 }
+
+var kenOrigPosLeft = -105;
+var kenOrigPosTop = 0;
+
+// ========== resets and end points =======
+function reboot() {
+    clearInterval(plasmaTimer);
+    clearInterval(tetsuTimer);
+    clearInterval(punchTimer);
+    $ken.removeClass('kick');
+    $ken.removeClass('punch');
+    $ken.removeClass('hadoken');
+    $ken.addClass('stance');
+    $ken.css('left', kenOrigPosLeft)
+    $ken.css('top', kenOrigPosTop)   
+    $result.css("display", "none");
+    $tryAgain.css("display", "none");
+    destroyCode();
+}
+function destroyCode() {
+    $code.val(""); 
+}
+
+function noCode() {
+    alert("Gimme yer code!");
+}
+
+function tryAgain() {
+    $tryAgain.css("display", "block");
+}
+
+//  ============== perfect ==============
 
 function perfect() {
     $ken.addClass('perfect');
@@ -34,22 +72,19 @@ function perfect() {
     $result.css("display", "block");
 }
 
+
+// ----------- hadoken and plasm ----------
+
 var plasmaDelay = 200;
 var plasmaDistance = 40;
 var plasmaTimer = null;
 var plasmaOrigPos = -50;
 
 
-
-function hadoken() {
-    $ken.addClass('hadoken');
-    setTimeout(function() { 
-        $plasma.addClass('plasma'); 
-        plasmaTimer = setInterval(function(){
-            plasmaMove();
-        }, plasmaDelay);  
-    }, 400);
-    setTimeout(function() { $ken.removeClass('hadoken'); }, 500);
+function resetPlasma() {
+    $plasma.removeClass('plasma');
+    $plasma.css('left', (plasmaOrigPos + 'px'));
+    clearInterval(plasmaTimer);
 }
 
 function plasmaMove() {
@@ -62,34 +97,42 @@ function plasmaMove() {
     }
 }
 
+function hadoken() {
+    $ken.addClass('hadoken');
+    setTimeout(function() { 
+        $plasma.addClass('plasma'); 
+        plasmaTimer = setInterval(function(){
+            plasmaMove();
+        }, plasmaDelay);  
+    }, 400);
+    setTimeout(function() { $ken.removeClass('hadoken'); }, 500);
+}
+
+
+// ============ tetsu kick ============
+
 var tetsuDelay = 100;
 var tetsuDistance = 40;
 var tetsuTimer = null;
-var kenOrigPos = -105;
 
 function tetsuMove() {
-    kenCurrentPos = parseInt($ken.css('left'));
-    $ken.css('left', ((kenCurrentPos + tetsuDistance) + 'px'));
+    kenCurrentPosLeft = parseInt($ken.css('left'));
+    $ken.css('left', ((kenCurrentPosLeft + tetsuDistance) + 'px'));
     if (parseInt($ken.css('left')) > 300) {
         tetsuDistance = -40
         destroyCode();
         tryAgain(); 
 
     } else if (parseInt($ken.css('left')) < -130) {
-        resetTetsu();
+        reboot();
+        tetsuDistance = 40
     }
 }
 
 function resetTetsu() {
     $ken.removeClass('kick');
-    $ken.css('left', (kenOrigPos + 'px'));
+    $ken.css('left', (kenOrigPosLeft + 'px'));
     clearInterval(tetsuTimer);
-}
-
-function resetPlasma() {
-    $plasma.removeClass('plasma');
-    $plasma.css('left', (plasmaOrigPos + 'px'));
-    clearInterval(plasmaTimer);
 }
 
 function tetsu() {
@@ -99,19 +142,75 @@ function tetsu() {
     }, tetsuDelay);
 }
 
-function destroyCode() {
-    $code.val(""); 
+
+
+
+//  ============== dragon punch ============
+var punchDelay = 100;
+var punchTimer = null;
+var punchDistance = -40;
+
+function resetPunch() {
+ $ken.removeClass('punch');
+ $ken.css('top', (kenOrigPosTop + 'px'));
+ clearInterval(punchTimer);   
 }
 
-function noCode() {
-    alert("Gimme yer code!");
+function punchMove() {
+    kenCurrentPosTop = parseInt($ken.css('top'));
+    $ken.css('top', ((kenCurrentPosTop + punchDistance) + 'px'));
+    if (parseInt($ken.css('top')) < -120) {
+        punchDistance = 40;
+        destroyCode();
+        tryAgain(); 
+
+    } else if (parseInt($ken.css('top')) > kenOrigPosTop) {
+        resetPunch();
+        punchDistance = -40;
+    }
 }
 
-function tryAgain() {
-    $tryAgain.css("display", "block");
-    setTimeout(function(){
-        $tryAgain.css("display", "none")}, 800
-        ); 
+function dragonPunch() {
+    $ken.addClass('punch');
+    punchTimer = setInterval(function() {
+        punchMove();
+    }, punchDelay)
+}
+
+
+
+//  ============== jump ============
+var jumpDelay = 100;
+var jumpTimer = null;
+var jumpUp = -40;
+var jumpRight = 40;
+
+
+
+function jumpMove() {
+    kenCurrentPosLeft = parseInt($ken.css('left'));
+    kenCurrentPosTop = parseInt($ken.css('top'));
+
+    $ken.css('left', ((kenCurrentPosLeft + jumpRight) + 'px'));
+    $ken.css('top', ((kenCurrentPosTop + jumpUp) + 'px'));
+
+    if (parseInt($ken.css('top')) < -120) {
+        jumpUp = 40
+    }
+    if (parseInt($ken.css('left')) > 220) {
+        $ken.removeClass('jump');
+       clearInterval(jumpTimer); 
+       dragonPunch();
+   }
+}
+
+
+
+function jump() {
+    $ken.addClass('jump');
+    jumpTimer = setInterval(function() {
+        jumpMove();
+    }, jumpDelay)
 }
 
 function codeCheck() {
@@ -121,15 +220,9 @@ function codeCheck() {
     //     noCode();
     // } else 
     if (random <= 0.25) {
-        $ken.addClass('punch');
-        setTimeout(function() { $ken.removeClass('punch'); }, 500);
-        destroyCode();
-        tryAgain();
+        jump();
     } else if ((random > 0.25) && (random <= 0.5) ) {
-        $ken.addClass('kick');
-        setTimeout(function() { $ken.removeClass('kick'); }, 500);
-        destroyCode();
-        tryAgain();
+        tetsu();
     } else if ((random > 0.25) && (random <= 0.75)) {
         hadoken();
     } else {
@@ -142,11 +235,7 @@ function codeCheck() {
     }
 }
 
-function reboot() {
-    $ken.addClass('stance');
-    $result.css("display", "none");
-    destroyCode();
-}
+
 
 
 
